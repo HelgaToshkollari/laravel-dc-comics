a9e3c7667d2850dbc765db3371bd4ece22ee138a
HelgaToshkollari <toshkollari.helga@gmail.com>
2023-01-26 20:54:49 +0100

    first commit

---
+++ database/migrations/2023_01_26_192402_create_comics_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("thumb");
+            $table->string("series");
+            $table->string("type");
+            $table->float("price", 8, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
